309393fa1a41c62cb04ff5e9b0d29ad7ffe40786
irregularBismuth <118374482+irregularBismuth@users.noreply.github.com>
2025-12-02 23:29:07 +0100

    update dependencies and start with parsing

---
+++ src/core/cards/green.rs
@@ -0,0 +1,5 @@
+use serde::Serialize;
+#[derive(Serialize)]
+struct GreenCard {
+    name: String,
+}
--- src/parsing/cards.rs
+++ src/parsing/cards.rs
@@ -1,2 +1,19 @@
-#[non_exhaustive]
-struct Cards {}
+use anyhow::Result;
+use std::path::Path;
+use tokio::fs;
+use tokio::io::{self, AsyncBufReadExt, BufReader};
+
+fn parse_line(line: impl AsRef<str>) {
+    todo!("parse the line with nom ")
+}
+
+pub async fn parse_cards(path: impl AsRef<Path>) -> Result<()> {
+    let file = fs::File::open(path).await?;
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines();
+
+    while let Some(line) = lines.next_line().await? {
+        parse_line(line);
+    }
+    Ok(())
+}
